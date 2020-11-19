39f094369b4b3680a08865699f328f750363b310
Mateus Félix <44587544+mateusfmfm@users.noreply.github.com>
2020-11-19 14:37:23 -0300

    Add files via upload

---
+++ Meals/README.md
@@ -0,0 +1,16 @@
+# Meals
+
+Aplicação de Receitas e Cozinha com base de dados simples para treinamento de navegação entre páginas com Routes e Pages widgets, utilização de push/pop/replace, Transferência de Dados via argumentos, uso e organização de Tabelas em pilhas, Drawers (menu lateral) e diálogos.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ Meals/lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:Meals/models/meal.dart';
+import 'package:Meals/screens/category_meals_screen.dart';
+import 'package:Meals/screens/filters_screen.dart';
+import 'package:Meals/screens/meal_detail_screen.dart';
+import 'package:Meals/screens/tabs_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import './dummy_data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    "gluten": false,
+    "lactose": false,
+    "vegan": false,
+    "vegetarian": false,
+  };
+
+  List<Meal> _avaiableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
+
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
+
+      _avaiableMeals = DUMMY_MEALS.where((meal) {
+        if (_filters["gluten"] && !meal.isGlutenFree) {
+          return false;
+        }
+        if (_filters["lactose"] && !meal.isLactoseFree) {
+          return false;
+        }
+        if (_filters["vegan"] && !meal.isVegan) {
+          return false;
+        }
+        if (_filters["vegetarian"] && !meal.isVegetarian) {
+          return false;
+        }
+        return true;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(String mealId) {
+    final existingIndex =
+        _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+
+    if (existingIndex >= 0) {
+      setState(() {
+        _favoriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        _favoriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool _isMealFavorite(String id) {
+    return _favoriteMeals.any((meal) => meal.id == id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+      title: 'Meals',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: "Relaway",
+        textTheme: ThemeData.light().textTheme.copyWith(
+            body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+            title: TextStyle(
+                fontSize: 20,
+                fontFamily: "RobotoCondensed",
+                fontWeight: FontWeight.bold)),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      // home: ,
+      routes: {
+        '/': (ctx) => TabsScreen(_favoriteMeals),
+        CategoryMealsScreen.routeName: (ctx) =>
+            CategoryMealsScreen(_avaiableMeals),
+        MealDetailScreen.routeName: (ctx) =>
+            MealDetailScreen(_toggleFavorite, _isMealFavorite),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(_filters, _setFilters),
+      },
+    );
+  }
+}
